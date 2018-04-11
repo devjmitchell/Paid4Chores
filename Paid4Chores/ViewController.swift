7befb3d3d4594bbc66c639c6d9e90cf97bd674e5
SemperPhoenix <semperphoenixaz@gmail.com>
2018-04-11 11:15:23 -0700

    Initial Commit

---
+++ Paid4Chores/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Paid4Chores
+//
+//  Created by Jason Mitchell on 4/11/18.
+//  Copyright © 2018 Jason Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
