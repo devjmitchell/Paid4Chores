0d5d4b0dcf86152ef38d29a5cf5466f19e522cfa
SemperPhoenix <semperphoenixaz@gmail.com>
2018-04-11 12:15:13 -0700

    Apply suggested warning fix for RealmSwift

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '9.0'
+
+target 'Paid4Chores' do
+  use_frameworks!
+
+  # Pods for Paid4Chores
+
+pod 'RealmSwift'
+
+end
---
+++ Pods/Target Support Files/Pods-Paid4Chores/Pods-Paid4Chores-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Paid4Chores : NSObject
+@end
+@implementation PodsDummy_Pods_Paid4Chores
+@end
